74a42b129bdaa777aafb635f06b04cfb5733b92e
Liam Fries <lfries@olin.edu>
2025-11-09 14:38:41 -0500

    set up top module

--- RISK5/top.sv
+++ RISK5/top.sv
@@ -1,96 +1,47 @@
-`include "values.sv" 
-`include "ws2812b.sv" // drives the ws2812b led strip
-`include "controller.sv" // finite state machine to control the data flow
+`include "memory.sv" 
+`include "program_counter.sv" 
+`include "register_file.sv" 
+`include "alu.sv"
 
-// led_matrix top level module
 
 module top(
     input logic     clk, 
-    input logic     SW, 
-    input logic     BOOT, 
-    output logic    _48b, 
-    output logic    _45a
+    output logic    RGB_R, 
+    output logic    RGB_G,
+    output logic    RGB_B,
+    output logic    LED
+
 );
-    // three data buses
-    logic [7:0] read_data;
-
-    logic [5:0] pixel;
-    logic newframe; 
-
-    // green red blue
-    logic [23:0] shift_reg = 24'd0; // going to put pixe
-    logic load_sreg;
-    logic transmit_pixel;
-    logic shift; 
-    logic ws2812b_out;
-
-
-
-    // my stuff
-
-    logic [63:0] pixel_control = 64'b0;
-    logic [63:0] next_pixel_control; // next state of the array 
-
-    logic [5:0] pixel_index = 3'b0; // pixel index to light up
-
-    logic [1:0] flipper = 2'b00; // controls which color is being loaded
     
-    
-    
-
-    // Instance sample memory for red channel
-    values u1 (
+    memory#(
+        .INIT_FILE      ("") // Assign at some point
+    ) u1(
         .clk         (clk), 
-        .rst_n       (SW), // <-- FIX: Connect the reset port!
-        .newframe    (newframe), 
-        .pixel        (pixel),
-        .read_data     (read_data)
+        .write_mem       (write_mem),
+        .funct3          (funct3), 
+        .write_address   (write_address), 
+        .write_data      (write_data), 
+        .read_address    (read_address),
+        .read_data       (read_data),
+        .led             (led),     
+        .red             (red),            
+        .green           (green),          
+        .blue            (blue)
     );
 
-    // Instance the WS2812B output driver
-    ws2812b u4 ( // this is the controller 
+    program_counter u2 (  
         .clk            (clk), 
-        .serial_in      (shift_reg[23]), // produces for one ck period
-        .transmit       (transmit_pixel), // goes high for 15 * 24 clk cycles cause that timing workes so it does not lock
-        .ws2812b_out    (ws2812b_out), //
-        .shift          (shift)
+        .pc_sel         (pc_sel),
+        .result         (result),
+        .pc             (pc)
     );
 
-    // Instance the controller
-    controller u5 (
+    register_file u3 (
         .clk            (clk), 
-        .load_sreg      (load_sreg), 
-        .transmit_pixel (transmit_pixel),// goes high for 15 * 24 clk cycles cause that timing workes so it does not lock 
-        .pixel          (pixel), // pixel address
-        .newframe          (newframe) // fram address
     );
 
-    always_ff @(posedge clk) begin
-        if(newframe) begin
-            flipper = flipper + 1;
-            if (flipper == 2'b11) begin
-                flipper = 2'b00;
-            end
-        end
-        if (load_sreg) begin // if shift reg is high
-            unique case (flipper)
-                2'b00:
-                    shift_reg <= { read_data, 16'd0 };
-                2'b01:
-                    shift_reg <= { 8'd0, read_data, 8'd0 };
-                2'b10:
-                    shift_reg <= { 16'd0, read_data };
-                2'b11: // this is the default case
-                    shift_reg <= { 16'd0, read_data };
-            endcase
-        end
-        else if (shift) begin
-            shift_reg <= { shift_reg[22:0], 1'b0 }; 
-        end
-    end
-
-
-    assign _48b = ws2812b_out; // output
-    assign _45a = ~ws2812b_out; // complementary output
+    alu u4 (
+        .clk            (clk), 
+    );
 
 endmodule
