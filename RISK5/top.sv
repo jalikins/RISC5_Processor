813ddc915c678956c47ee76d3d3e581355224501
Liam Fries <lfries@olin.edu>
2025-11-09 15:23:38 -0500

    top updates

--- RISK5/top.sv
+++ RISK5/top.sv
@@ -10,9 +10,22 @@ module top(
     output logic    RGB_G,
     output logic    RGB_B,
     output logic    LED
-
 );
-    
+
+    parameter RTYPECODE = 7'b0110011;
+
+    logic[31:0] current_instr;
+    logic[6:0] op_code;
+    logic[2:0] funct3;
+    logic[6:0] funct7;
+    logic[5:0] rs1;
+    logic[5:0] rs2;
+    logic[5:0] rd;
+    logic[11:0] immed12;
+    logic[19:0] immed20;
+
+    logic load_new; // Signal to load a new instruction 
+
     memory#(
         .INIT_FILE      ("") // Assign at some point
     ) u1(
@@ -42,6 +55,26 @@ module top(
 
     alu u4 (
         .clk            (clk), 
+        .rs1_data       (rs1_data),
+        .rs2_data       (rs2_data),
+        .immed12        (immed12),
+        .pc             (pc), // for jump/branch
+        .funct3         (funct3),
+        .funct7         (funct7),
+        .result         (result)
     );
+
+    always_ff @(posedge clk) begin
+        if (load_new == 1) begin
+            op_code = current_instr[25:31]
+            funct3 = current_instr[17:19]
+            if (op_code == RTYPECODE) begin
+                funct7 = current_instr[0:6]
+                rs2 = current_instr[7:11]
+                rs1 = current_instr[12:16]
+                rd = current_instr[20:24] // R-type instructions ???
+            end
+        end
+    end
 
 endmodule
