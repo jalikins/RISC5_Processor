0c39f87646dca1de168f52159d69fc5b76bb2862
Jacob Likins <jalikins05@gmail.com>
2025-11-29 14:41:12 -0500

    changed controller and finished ALU_unit. and Deleted ALU_controller

--- RISK5/ALU_unit.sv
+++ RISK5/ALU_unit.sv
@@ -7,15 +7,20 @@ module ALU_unit(
 );
     always_comb begin
         case (alu_control)
-            4'b0000: alu_result = A & B; // AND
-            4'b0001: alu_result = A | B; // OR
-            4'b0010: alu_result = A + B; // ADD
-            4'b0110: alu_result = A - B; // SUBTRACT
-            4'b0111: alu_result = (A < B) ? 1 : 0; // SET LESS THAN
-            4'b1100: alu_result = ~(A | B); // NOR
-            default: alu_result = 32'b0; // DEFAULT
+           4'b0010: alu_result = A + B; // ADD
+           4'b0110: alu_result = A - B; // SUB
+           4'b0000: alu_result = A & B; // AND
+           4'b0001: alu_result = A | B; // OR
+           4'b0011: alu_result = A ^ B; // XOR
+           4'b0100: alu_result = A << B[4:0]; // SLL
+           4'b0101: alu_result = A >> B[4:0]; // SRL
+           4'b0111: alu_result = $signed(A) >>> B[4:0]; // SRA 
+           4'b1100: alu_result = (A < B) ? 32'b1 : 32'b0; // SLT wrong need to figure out
+
+            default: alu_result = 32'b0;
         endcase
 
+        // zero flag
         zero = (alu_result == 32'b0) ? 1'b1 : 1'b0;
     end
 
