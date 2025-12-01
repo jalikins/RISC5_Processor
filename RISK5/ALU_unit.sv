af8218a121abb200155071bcc80f249a2d7dc627
Liam Fries <lfries@olin.edu>
2025-12-01 11:30:10 -0500

    standardise alu signals and implemented some edge cases

--- RISK5/ALU_unit.sv
+++ RISK5/ALU_unit.sv
@@ -1,18 +1,23 @@
 module ALU_unit(
-    input [31:0] A,
-    input [31:0] B,
+    input logic[31:0] A,
+    input logic[31:0] B,
     input [3:0] alu_control,
     output reg zero,
     output reg[31:0] alu_result
 );
     always_comb begin
         case (alu_control)
-            4'b0000: alu_result = A & B; // AND
-            4'b0001: alu_result = A | B; // OR
-            4'b0010: alu_result = A + B; // ADD
-            4'b0110: alu_result = A - B; // SUBTRACT
-            4'b0111: alu_result = (A < B) ? 1 : 0; // SET LESS THAN
+            4'b0000: alu_result = $signed(A + B); // ADD (signed)
+            4'b0110: alu_result = $signed(A - B); // SUBTRACT
+            4'b0010: alu_result = A | B; // OR
+            4'b0001: alu_result = A & B; // AND
+            4'b0100: alu_result = A << B[4:0]; // SLL
+            4'b0101: alu_result = A >> B[4:0]; // SRL
+            4'b0111: alu_result = $signed(A) >>> B[4:0]; // SRA
+            4'b1001: alu_result = (A < B) ? 32'b1 : 32'b0; // SET LESS THAN Unsinged
+            4'b1010: alu_result = ($signed(A) < $signed(B)) ? 32'b1 : 32'b0; // SLT signed
             4'b1100: alu_result = ~(A | B); // NOR
+            4'b1101: alu_result = A - B; // Subtract unsigned
             default: alu_result = 32'b0; // DEFAULT
         endcase
 
