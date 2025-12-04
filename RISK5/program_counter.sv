8969892438ff8f9cc091f3048b6f9f1272d836df
Liam Fries <lfries@olin.edu>
2025-12-04 16:16:39 -0500

    fixed old_pc

--- RISK5/program_counter.sv
+++ RISK5/program_counter.sv
@@ -2,13 +2,13 @@ module program_counter(
     input logic clk,
     input logic PCUpdate,
     input logic[31:0] result,
-    input logic[31:0] old_pc,
+    output logic[31:0] old_pc,
     output logic[31:0] current_pc
 );
 
     initial begin
         current_pc = 32'b0; // lowest point in instr memory
-        old_pc = current_pc;
+        old_pc = 32'b0;
     end
     
     always_ff@(posedge clk) begin
